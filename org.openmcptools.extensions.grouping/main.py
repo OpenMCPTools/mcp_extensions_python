c80376141bdb49de7e878dee4dcdacb6d8ada7c1
Scott Lewis <scottslewis@gmail.com>
2026-03-13 15:38:56 -0700

    Added grouping extension

---
+++ org.openmcptools.extensions.grouping/main.py
@@ -0,0 +1,7 @@
+from org.openmcptools.extensions.grouping import GroupSchema
+def main():
+    print("Hello from org-openmcptools-extensions-grouping!")
+    schema = GroupSchema(name="agroup")
+
+if __name__ == "__main__":
+    main()
