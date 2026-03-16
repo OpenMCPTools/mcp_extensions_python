af35e79e6609f584714765030c3ba914c11236b9
Scott Lewis <scottslewis@gmail.com>
2026-03-15 18:56:44 -0700

    Added mcptoolgroups module.  Removed grouping extension

--- org.openmcptools.extensions.grouping/main.py
+++
@@ -1,7 +0,0 @@
-from org.openmcptools.extensions.grouping import GroupSchema
-def main():
-    print("Hello from org-openmcptools-extensions-grouping!")
-    schema = GroupSchema(name="agroup")
-
-if __name__ == "__main__":
-    main()
